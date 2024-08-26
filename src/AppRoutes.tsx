be61389b59a319809f69a4d7e982850709299897
Kapitan-Black <164235336+Kapitan-Black@users.noreply.github.com>
2024-08-26 16:17:53 +0300

    a

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Layout from "./layout/Layout";
 import BidFormPage from "./pages/BidFormPage";
