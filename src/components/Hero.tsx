bf5b10686778d2e211de741152ae78e4ca3402cc
Kapitan-Black <ikletzko@gmail.com>
2024-09-27 00:06:41 +0700

    a

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,15 @@
 import hero from "../assets/HeroToursSite2.png";
+import logo from "../assets/logo-thailand-sababa1.png";
+
 
 const Hero = () => {
   return (
-    <div className="sm:-mx-12">
+    <div className=" sm:-mx-12">
       <img src={hero} className="w-full h-[500px] object-cover" />
+      <img
+        src={logo}
+        className="absolute sm:w-[300px] w-[200px] top-1/3 sm:top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      />
     </div>
   );
 };
