87d3c568cae2b0f0d6a76579929b5ef956f33ec3
Kapitan-Black <ikletzko@gmail.com>
2024-09-27 00:15:57 +0700

    a

--- src/components/readyBids/BeforeApproved.tsx
+++ src/components/readyBids/BeforeApproved.tsx
@@ -6,7 +6,7 @@ interface BeforeApprovedProps {
 
 const BeforeApproved: React.FC<BeforeApprovedProps> = ({formName, randomNumber}) => {
     return (
-      <div dir="rtl" className="flex justify-center mb-8 -mt-12 h-[150px]">
+      <div dir="rtl" className="flex justify-center mb-8 -mt-12 h-[150px] px-4">
         <div className=" bg-blue-50 gap-2 p-4 rounded-md w-[500px] shadow-md">
           <h2 className="text-center mb-4 text-lg">{formName}</h2>
           <p className="text-center md:text-xl">ההצעה שלכם כבר מוכנה</p>
