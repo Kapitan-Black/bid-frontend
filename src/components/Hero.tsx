a32b5055bb221337fa365329057e13c5b23f9ad9
Kapitan-Black <ikletzko@gmail.com>
2024-09-27 00:10:58 +0700

    a

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <img src={hero} className="w-full h-[500px] object-cover" />
       <img
         src={logo}
-        className="absolute sm:w-[300px] w-[200px] top-1/3 sm:top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        className="absolute sm:w-[300px] w-[200px] top-[200px] sm:top-[200px] left-1/2 transform -translate-x-1/2 -translate-y-1/2"
       />
     </div>
   );
